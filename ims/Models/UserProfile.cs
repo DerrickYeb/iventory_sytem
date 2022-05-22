74a3932705c5cff6648213a2bf3a247f9933dca9
Derrick Yeboah <derrickyeboah999@hotmail.com>
2022-05-22 19:42:45 +0000

    upgraded from core 2.1 to dotnet core 6

---
+++ ims/Models/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ims.Models
+{
+    public class UserProfile
+    {
+        [Key]
+        public int UserProfileId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string OldPassword { get; set; }
+        public string ProfilePicture { get; set; } = "/upload/blank-person.png";
+
+        public string ApplicationUserId { get; set; }
+    }
+}
